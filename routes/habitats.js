f76795361f5bf0214329aa15e2cd4fe6f8db409d
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2018-12-28 12:26:01 -0800

    outline for habitats routes

--- routes/habitats.js
+++ routes/habitats.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const router = new express.Router();
 // const jwt = require('jsonwebtoken');
 // const env = require('../config');
@@ -6,7 +7,13 @@ const router = new express.Router();
 // Shows all habitats available
 router.get('/', async function(req, res, next) {
   try {
-    return res.json('bleh');
+    let habitats = Habitat.get();
+    if (habitats.length > 1) {
+      // process stuff and hand back json of all avail habitats
+    } else {
+      let results = await axios.get('//PokemonUrl');
+      return res.json(results);
+    }
   } catch (error) {
     return next(error);
   }
@@ -15,7 +22,9 @@ router.get('/', async function(req, res, next) {
 // Shows the pokemon that can be battled within a habitat
 router.get('/:habitat', async function(req, res, next) {
   try {
-    return res.json('bleh');
+    // Get all Pokemon inside the current habitat.
+    let results = await axios.get('//PokemonUrl');
+    return res.json(results);
   } catch (error) {
     return next(error);
   }
@@ -24,7 +33,9 @@ router.get('/:habitat', async function(req, res, next) {
 // Trigger a wild pokemon encounter (returns a pokemon instance)
 router.get('/:habitat/battle', async function(req, res, next) {
   try {
-    return res.json('bleh');
+    // get the data for a specific pokemon and send it to the user
+    let results = await axios.get('//PokemonUrl');
+    return res.json(results);
   } catch (error) {
     return next(error);
   }
